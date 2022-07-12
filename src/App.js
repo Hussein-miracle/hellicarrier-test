0a0025dd26f9fdb3e88e9bec456db1a938b9d78c
Abdullahi Hussein <miraacle64@gmail.com>
2022-07-12 01:10:20 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
+import Checkout from './routes/checkout/checkout.component';
+
+
+
+
+
+const App = () => {
+  return (
+    <div>
+      Heli Carrier App
+    </div>
+  );
+};
+
+export default App;
