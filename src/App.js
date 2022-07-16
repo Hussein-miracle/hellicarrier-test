d3f948a9319a6a3c3e6ca52e2e8d4acf77ac0b7e
Abdullahi Hussein <miraacle64@gmail.com>
2022-07-16 01:53:48 +0100

    fix: made the code dry

--- src/App.js
+++ src/App.js
@@ -1,82 +1,118 @@
-import { useContext,useState  } from "react";
+import { useContext, useState } from "react";
 import { JobsContext } from "./contexts/jobs.context";
 import Container from "./components/container/container";
-import "./header.scss";
 import "./App.scss";
 
-
 const App = () => {
-  const { loading, error, jobs} = useContext(JobsContext);
-  const [query,setQuery] = useState("");
+  const { loading, error, jobs } = useContext(JobsContext);
+  const [query, setQuery] = useState("");
 
-
+  const Filters = [
+    {
+      identifier: "",
+      name: "All",
+    },
+    {
+      identifier: "backend",
+      name: "Backend",
+    },
+    {
+      identifier: "frontend",
+      name: "Frontend",
+    },
+    {
+      identifier: "node.js",
+      name: "node.js",
+    },
+    {
+      identifier: "react",
+      name: "react",
+    },
+    {
+      identifier: "typescript",
+      name: "typescript",
+    },
+    {
+      identifier: "graphql",
+      name: "graphql",
+    },
+    {
+      identifier: "sql",
+      name: "sql",
+    },
+  ];
   const handleQuery = (q) => {
-    const input  = q.target.value.trim().toLowerCase();
+    const input = q.target.value.trim().toLowerCase();
     setQuery(input);
   };
 
+  const onFilterClick = (filter) => {
+    setQuery(filter);
+  };
 
-  const filterNone = () => {
-    setQuery('')
-  }
-  const filterBE = () => {
-    setQuery('backend')
-  }
-  const filterFrontend = () => {
-    setQuery('frontend')
-  }
-  const filterNode = () => {
-    setQuery('node.js')
-  }
-  const filterReact = () => {
-    setQuery('react')
-  }
-  const filterType = () => {
-    setQuery('typescript')
-  }
-  const filterGRAPH = () => {
-    setQuery('graphql')
-  }
-  const filterSQL = () => {
-    setQuery('sql')
-  }
-
-
+  // const filterNone = () => {
+  //   setQuery('')
+  // }
+  // const filterBE = () => {
+  //   setQuery('backend')
+  // }
+  // const filterFrontend = () => {
+  //   setQuery('frontend')
+  // }
+  // const filterNode = () => {
+  //   setQuery('node.js')
+  // }
+  // const filterReact = () => {
+  //   setQuery('react')
+  // }
+  // const filterType = () => {
+  //   setQuery('typescript')
+  // }
+  // const filterGRAPH = () => {
+  //   setQuery('graphql')
+  // }
+  // const filterSQL = () => {
+  //   setQuery('sql')
+  // }
 
   const filteredJobs = jobs?.filter((job) => {
-    return (job.title?.toLowerCase().includes(query) || job?.tags.filter((tag) => tag?.toLowerCase().includes(query)).join("") || job?.location?.toLowerCase().includes(query) )
+    return (
+      job.title?.toLowerCase().includes(query) ||
+      job?.tags.filter((tag) => tag?.toLowerCase().includes(query)).join("") ||
+      job?.location?.toLowerCase().includes(query)
+    );
   });
 
-
-
-  
   return (
     <div className="App">
-    <section className="header">
-      <div className="header__top">
-        <div className="header__logo">Helli-Careers</div>
-        <input
-          className="search__input"
-          type="text"
-          // value={query}
-          onChange={handleQuery}
-          placeholder="Search jobs by role, company , skills "
-        />
-      </div>
-      <div className="header__bottom">
-        <div className="header__bottom--title">Filters :</div>
-        <ul className="header__bottom--filters">
-          <li className={`filter ${query === '' && 'clicked'}`} onClick={filterNone}>All</li>
-          <li className={`filter ${query === 'backend' && 'clicked'}`} onClick={filterBE}>Backend</li>
-          <li className={`filter ${query === 'frontend' && 'clicked'}`} onClick={filterFrontend}>Frontend</li>
-          <li className={`filter ${query === 'node.js' && 'clicked'}`} onClick={filterNode}>Node.js</li>
-          <li className={`filter ${query === 'react' && 'clicked'}`} onClick={filterReact}>React</li>
-          <li className={`filter ${query === 'typescript' && 'clicked'}`} onClick={filterType}>Typescript</li>
-          <li className={`filter ${query === 'graphql' && 'clicked'}`} onClick={filterGRAPH}>GRAPHQL</li>
-          <li className={`filter ${query === 'sql' && 'clicked'}`} onClick={filterSQL}>SQL</li>
-        </ul>
-      </div>
-    </section>
+      <section className="header">
+        <div className="header__top">
+          <div className="header__logo">Helli-Careers</div>
+          <input
+            className="search__input"
+            type="text"
+            // value={query}
+            onChange={handleQuery}
+            placeholder="Search jobs by role, company , skills "
+          />
+        </div>
+        <div className="header__bottom">
+          <div className="header__bottom--title">Filters :</div>
+          <ul className="header__bottom--filters">
+            {Filters.map(({ identifier, name },index) => {
+              return (
+                <li
+                key={index+name+identifier}
+                  className={`filter ${query === identifier && "clicked"}`}
+                  onClick={() => onFilterClick(identifier)}
+                >
+                  {name}
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </section>
       <Container jobs={filteredJobs} loading={loading} error={error} />
     </div>
   );
--- src/components/container/container.jsx
+++ src/components/container/container.jsx
@@ -6,12 +6,19 @@ import Spinner from "../spinner/spinner";
 import "./container.scss";
 const Container = ({ jobs, loading, error }) => {
 
+  if(navigator.onLine === false){
+    return <h2 className="network"
+  >
+    You need to be connected to be able to view this site's content
+  </h2>
+  }
+
   if (loading) {
     return <Spinner />;
   }
 
   if (error) {
-    return <p className="error">Error Fetching Jobs</p>;
+    return <h1 className="error">Error Fetching Jobs</h1>;
   }
 
   return (
